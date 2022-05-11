d0e488974f17132bc4f120fdec2cc46531c719f7
callmefish <yzy970214@outlook.com>
2022-05-11 10:03:29 -0400

    No logging system

--- app/application.go
+++ app/application.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"github.com/callmefish/bookstore_users-api/logger"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,5 +11,7 @@ var (
 
 func StartApplication() {
 	mapUrls()
+
+	logger.Info("about tot start the application...")
 	router.Run(":8080")
 }
